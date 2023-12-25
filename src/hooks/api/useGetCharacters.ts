6cfdab8702b5c0a07e37f97ee1fff9afc0bfe356
Batur Akkurt <ta2ulk@batur.io>
2023-12-26 02:34:02 +0300

    fix: type

--- src/hooks/api/useGetCharacters.ts
+++ src/hooks/api/useGetCharacters.ts
@@ -3,18 +3,18 @@ import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import type { CharacterFilter, Character, Info } from 'rickmortyapi';
 import { getCharacters } from 'rickmortyapi';
 
-type Param = Omit<CharacterFilter, 'status' | 'gender'> & {
+type Params = Omit<CharacterFilter, 'status' | 'gender'> & {
   status?: 'Dead' | 'Alive' | 'unknown';
   gender?: 'Female' | 'Male' | 'Genderless' | 'unknown';
 };
 
 interface UseGetCharactersProps extends QueryOptions<Info<Character[]>, Error> {
-  params?: Partial<Param>;
+  params?: Params;
 }
 
 const useGetCharacters = ({ params, ...props }: UseGetCharactersProps = {}) => {
   return useQuery({
-    queryKey: ['characters', params, ...Object.values(params ?? {})],
+    queryKey: ['characters', params],
     queryFn: async () => (await getCharacters(params)).data,
     placeholderData: keepPreviousData,
     ...props
