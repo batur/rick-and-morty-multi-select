aa8634ca9be4fb179ee20a2d105e4f70bd3fb68f
Batur Akkurt <ta2ulk@batur.io>
2023-12-26 02:24:27 +0300

    refactor: type omitting

--- src/hooks/api/useGetCharacters.ts
+++ src/hooks/api/useGetCharacters.ts
@@ -3,15 +3,11 @@ import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import type { CharacterFilter, Character, Info } from 'rickmortyapi';
 import { getCharacters } from 'rickmortyapi';
 
-type Modify<T, R> = Omit<T, keyof R> & R;
-interface Param
-  extends Modify<
-    CharacterFilter,
-    {
-      status: 'Dead' | 'Alive' | 'unknown';
-      gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
-    }
-  > {}
+type Param = Omit<CharacterFilter, 'status' | 'gender'> & {
+  status?: 'Dead' | 'Alive' | 'unknown';
+  gender?: 'Female' | 'Male' | 'Genderless' | 'unknown';
+};
+
 interface UseGetCharactersProps extends QueryOptions<Info<Character[]>, Error> {
   params?: Partial<Param>;
 }
