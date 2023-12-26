493062be834745e45aa34af3b1be5419a9b114b5
Batur Akkurt <ta2ulk@batur.io>
2023-12-26 14:35:15 +0300

    feat: basic select

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import Select from 'react-select';
+
+import { api } from './hooks';
+
 const App: React.FC = () => {
+  const { data } = api.useGetCharacters();
+
   return (
     <>
       <div>
-        <p>Test</p>
+        <Select
+          options={data?.results?.map((character) => ({
+            label: character.name,
+            value: character.id
+          }))}
+          isMulti
+          isSearchable
+          onChange={(value) => {
+            console.log(value);
+          }}
+        />
       </div>
     </>
   );
