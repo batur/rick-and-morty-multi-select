c687b6b91f04d535442f1c72ddaaedfdceacfb38
Batur Akkurt <ta2ulk@batur.io>
2023-12-26 23:26:30 +0300

    refactor: basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
-import Select from 'react-select';
-import { type Character } from 'rickmortyapi';
-
+import { CustomSelect } from './components';
 import { api, useDebounce } from './hooks';
 
 const App: React.FC = () => {
   const [search, setSearch] = useState('');
   const { debouncedValue, isLoading: isDebounceLoading } = useDebounce(search, 500);
+
   const { data, isLoading } = api.useGetCharacters({
     params: {
       name: debouncedValue
     }
   });
 
-  const resetSearchValue = React.useCallback(() => {
-    setTimeout(() => {
-      setSearch('');
-    }, 500);
-  }, []);
-
-  const handleLabelWithSearchValue = useCallback(
-    (label: string) => {
-      return label.toLocaleLowerCase().replace(search.toLocaleLowerCase(), `<strong>${search}</strong>`);
-    },
-    [search]
-  );
-
   return (
     <>
       <main className='p-4'>
         <section className='flex w-full justify-center md:px-12 lg:max-w-[480px]'>
-          <Select
-            className='w-full'
-            options={data?.results?.map((character: Character) => ({
-              value: character.id,
-              label: character.name,
-              img: character.image
-            }))}
-            onInputChange={(value) => {
-              setSearch(value);
-            }}
-            components={{
-              Option: (option) => {
-                return (
-                  <div key={option.data.value} className={`mb-2 w-full ${option.isFocused && 'bg-slate-100'}`}>
-                    <button
-                      className='flex w-full flex-1 flex-row items-center text-sm text-gray-500'
-                      onClick={() => {
-                        option.selectOption(option.data);
-                        resetSearchValue();
-                      }}>
-                      <img src={option.data.img} alt={option.data.label} className='mr-2 h-8 w-8 rounded-sm' />
-                      <p
-                        className='capitalize'
-                        dangerouslySetInnerHTML={{
-                          __html: handleLabelWithSearchValue(option.data.label)
-                        }}
-                      />
-                    </button>
-                  </div>
-                );
-              }
-            }}
-            isLoading={isLoading || isDebounceLoading}
-            isClearable
-            isMulti
-            isSearchable
-          />
+          <CustomSelect search={search} data={data?.results ?? []} isLoading={isLoading || isDebounceLoading} setSearch={setSearch} />
         </section>
       </main>
     </>
---
+++ src/components/CustomSelect/index.tsx
@@ -0,0 +1,126 @@
+import React, { useCallback, memo } from 'react';
+
+import cn from 'classnames';
+import Select from 'react-select';
+import { type Character } from 'rickmortyapi';
+
+interface Props {
+  search: string;
+  data: Character[];
+  isLoading: boolean;
+  setSearch: (value: string) => void;
+}
+const CustomSelect: React.FC<Props> = ({ search, data, isLoading, setSearch }) => {
+  const resetSearchValue = React.useCallback(() => {
+    setTimeout(() => {
+      setSearch('');
+    }, 500);
+  }, []);
+
+  const handleLabelWithSearchValue = useCallback(
+    (label: string) => {
+      return label.toLocaleLowerCase().replace(search.toLocaleLowerCase(), `<strong>${search}</strong>`);
+    },
+    [search]
+  );
+
+  return (
+    <Select
+      styles={{
+        menuList: (provided) => ({
+          ...provided,
+          borderWidth: 1,
+          borderColor: '#94A3B8',
+          borderRadius: '0.375rem'
+        }),
+        multiValue: (provided) => ({
+          ...provided,
+          color: '#334155',
+          backgroundColor: '#E2E8F0',
+          borderRadius: '0.375rem',
+          padding: '0.1rem 0.325rem',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }),
+        multiValueRemove: (provided) => ({
+          ...provided,
+          backgroundColor: '#94A3B8',
+          margin: 4,
+          aspectRatio: 1,
+          color: '#F8FAFC',
+          borderRadius: '0.375rem'
+        }),
+        input: (provided) => ({
+          ...provided,
+          height: '2.125rem',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }),
+        container: (provided) => ({
+          ...provided,
+          width: '100%'
+        }),
+        valueContainer: (provided) => ({
+          ...provided,
+          padding: '0.215rem'
+        }),
+        control: (provided) => ({
+          ...provided,
+          borderRadius: '0.75rem',
+          borderWidth: 1,
+          borderColor: '#94A3B8'
+        }),
+        indicatorSeparator: () => ({
+          display: 'none'
+        })
+      }}
+      className='w-full'
+      options={data.map((character: Character) => ({
+        value: character.id,
+        label: character.name,
+        img: character.image
+      }))}
+      hideSelectedOptions={false}
+      onInputChange={(value) => {
+        setSearch(value);
+      }}
+      components={{
+        Option: (option) => {
+          return (
+            <button
+              className={cn(
+                'flex w-full flex-1 flex-row items-center text-sm text-gray-500 p-2 border-b border-slate-300',
+                option.isFocused && 'bg-slate-100'
+              )}
+              key={option.data.value}
+              role='option'
+              aria-selected={option.isSelected}
+              onClick={() => {
+                option.selectOption(option.data);
+                resetSearchValue();
+              }}>
+              <input type='checkbox' className='mr-2' checked={option.isSelected} readOnly />
+              <img src={option.data.img} alt={option.data.label} className='mr-2 h-8 w-8 rounded-lg' />
+              <p
+                className='capitalize'
+                dangerouslySetInnerHTML={{
+                  __html: handleLabelWithSearchValue(option.data.label)
+                }}
+              />
+            </button>
+          );
+        }
+      }}
+      menuIsOpen={process.env.NODE_ENV === 'development' ? true : undefined}
+      isLoading={isLoading}
+      isMulti
+      isClearable={false}
+      isSearchable
+    />
+  );
+};
+
+const MemoizedCustomSelect = memo(CustomSelect);
+export default MemoizedCustomSelect;
