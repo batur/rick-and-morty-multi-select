2677d4ad35ce08928d7a528a9628f7e67b933844
Batur Akkurt <ta2ulk@batur.io>
2023-12-26 19:43:23 +0300

    feat: basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,53 @@ import Select from 'react-select';
 import { api } from './hooks';
 
 const App: React.FC = () => {
+  const [search, setSearch] = React.useState('');
+
   const { data } = api.useGetCharacters();
 
   return (
     <>
-      <div>
-        <Select
-          options={data?.results?.map((character) => ({
-            label: character.name,
-            value: character.id
-          }))}
-          isMulti
-          isSearchable
-          onChange={(value) => {
-            console.log(value);
-          }}
-        />
-      </div>
+      <main className='p-4'>
+        <section className='flex w-full justify-center md:px-12 lg:max-w-[480px]'>
+          <Select
+            className='w-full'
+            options={data?.results?.map((character) => ({
+              label: character.name,
+              value: character.id,
+              img: character.image
+            }))}
+            onInputChange={(value) => {
+              setSearch(value);
+            }}
+            components={{
+              Option: (option) => {
+                return (
+                  <div {...option} className='mb-2 w-full'>
+                    <button
+                      className='flex w-full flex-1 flex-row items-center text-sm text-gray-500'
+                      onClick={() => {
+                        option.selectOption(option.data);
+                      }}>
+                      <img src={option.data.img} alt={option.data.label} className='mr-2 h-8 w-8 rounded-sm' />
+                      <p
+                        className='capitalize'
+                        dangerouslySetInnerHTML={{
+                          __html: option.data.label.toLowerCase().replace(search.toLowerCase(), `<strong>${search}</strong>`)
+                        }}
+                      />
+                    </button>
+                  </div>
+                );
+              }
+            }}
+            isMulti
+            isSearchable
+            onChange={(value) => {
+              console.log(value);
+            }}
+          />
+        </section>
+      </main>
     </>
   );
 };
